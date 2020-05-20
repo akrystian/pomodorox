82916773724639e5d3dc51ce52246ce3d079f36d
akrystian <akrystian@gmail.com>
2020-05-20 21:57:28 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Navbar} from "react-bootstrap";
+
+function Footer() {
+  return (
+    <Navbar bg="dark" variant="dark" sticky="bottom">
+      PomidoroX @ 2020
+    </Navbar>
+  )
+}
+
+export default Footer;
