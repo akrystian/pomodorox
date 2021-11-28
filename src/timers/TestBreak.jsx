33e727abcb1a605e32d0689a353474965bc6ed4f
akrystian <akrystian@gmail.com>
2021-11-29 00:46:30 +0100

    fix for reminder

--- src/timers/TestBreak.jsx
+++ src/timers/TestBreak.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import { NewTimer } from './counter/NewTimer'
 
 function TestBreak(showNotifications) {
-    return <NewTimer timerSeconds={5} showNotifications={showNotifications} />
+    return (
+        <NewTimer
+            timerSeconds={10}
+            showNotifications={showNotifications}
+            reminderSeconds={10}
+            debug={true}
+        />
+    )
 }
 
 export default TestBreak
