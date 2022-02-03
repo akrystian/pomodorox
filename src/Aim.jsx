3aee29c2a6761b6b21cfa6f17599abe3f9f28097
adamski.pro <akrystian@gmail.com>
2022-02-04 00:14:05 +0100

    remove task (#44)

--- src/Aim.jsx
+++ src/Aim.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import { Button, Card } from 'react-bootstrap'
+import { FaTrash } from 'react-icons/fa'
 
-export const Aim = ({ label }) => {
+export const Aim = ({ index, label, deleteHook }) => {
     const states = ['todo', 'working', 'done']
     const colors = ['secondary', 'primary', 'success']
     const [isTodo, setTodo] = useState(0)
@@ -20,7 +21,12 @@ export const Aim = ({ label }) => {
                         variant={colors[isTodo]}
                     >
                         {states[isTodo]}
-                    </Button>&nbsp;&nbsp;
+                    </Button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <Button variant="danger" onClick={() => deleteHook(index)}>
+                        <FaTrash />
+                    </Button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
                     {label}
                 </div>
             </Card.Body>
