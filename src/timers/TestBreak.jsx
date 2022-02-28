3a6de58cb450b774c92e4d2ca87bd4b3f1f1fd7b
akrystian <akrystian@gmail.com>
2022-02-28 23:42:44 +0100

    autocont regular

--- src/timers/TestBreak.jsx
+++ src/timers/TestBreak.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { NewTimer } from './counter/NewTimer'
 
-function TestBreak({ showNotifications, debugMode }) {
+function TestBreak({ showNotifications, debugMode, autoCount }) {
     return (
         <NewTimer
             timerSeconds={10}
             showNotifications={showNotifications}
             reminderSeconds={10}
             debug={debugMode}
+            autoCount={autoCount}
         />
     )
 }
