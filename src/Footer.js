06ae8d3cb64c70a13d6bd80d8cd7cbdb14bd2526
adamski.pro <akrystian@gmail.com>
2022-05-30 22:27:24 +0200

    license (#65)

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,43 @@
 import React from 'react'
 import { Navbar, Button } from 'react-bootstrap'
+import axios from 'axios'
 
-function Footer({ setDebugMode }) {
-    return (
-        <Navbar bg="light" variant="light">
-            PomidoroX &copy; 2021
-            <Button variant="link" onClick={() => setDebugMode(true)} size="sm">
+class Footer extends React.Component {
+    state = {
+        license: ""
+    }
+
+    async componentDidMount() {
+        const license = await axios.get('https://raw.githubusercontent.com/akrystian/pomodorox/master/LICENSE')
+            .then((response) =>
+                response.data
+                    .split(`\n`)
+                    .filter((_, index) => index === 2)
+                    .at(0)
+
+            )
+            .catch((error) => {
+                console.log(error);
+                return 'PomidoroX'
+            });
+
+        await this.setStateAsync({ license: license })
+    }
+
+    setStateAsync(state) {
+        return new Promise((resolve) => {
+            this.setState(state, resolve)
+        });
+    }
+
+    render() {
+        return <Navbar bg="light" variant="light" >
+            {this.state.license}
+            < Button variant="link" onClick={() => this.props.setDebugMode(true)} size="sm" >
                 .
-            </Button>
-        </Navbar>
-    )
+            </Button >
+        </Navbar >
+    }
 }
 
 export default Footer
